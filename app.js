0dd594fed19abec2496a4059b5a1d0080230f2c6
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2019-09-11 15:59:51 +0200

    eventlistner for blick and enter

--- app.js
+++ app.js
@@ -18,8 +18,26 @@ var UIController = (function(){
 // GLOBAL-APP-CONTROLLER
 var controller = (function(budgetCtrl,UICtrl){
 
-    document.querySelector(".add__btn").addEventListener("click",function(){
+    var ctrlAddItem = function (){
+        // 1.get input data from fields
         
+        // 2.add item to budget controller
+        
+        // 3.add new item to UI 
+        
+        // 4.calc budget
+        
+        // 5.display budget in UI
+    }
+
+    document.querySelector(".add__btn").addEventListener("click",ctrlAddItem);
+    
+    // Global event, from anywhere in the doucment.
+    document.addEventListener("keypress",function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        } 
+       
     });
     
 })(budgetController,UIController);
