211830c8409a0ae1b7e356809028af911c72d512
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2019-09-11 10:12:03 +0200

    added module pattern with a upcoming budgetcontroller

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// this is the starter js file..
+// this is the starter js file.. Module pattern
+
+var budgetController = (function(){
+    var x = 23;
+    var addTwoNumbers = function (y){
+        return x + y;
+    }
+    return {
+         publicTest: function(b){
+            console.log(addTwoNumbers(b));
+         }
+    }
+})();
