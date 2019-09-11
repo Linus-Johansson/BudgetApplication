e6cd6cf84f662ae876dda5d0b5559ffa1011a71a
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2019-09-11 15:18:47 +0200

    removed test code

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 // this is the starter js file.. Module pattern
 
 var budgetController = (function(){
-    var x = 23;
-    var addTwoNumbers = function (y){
-        return x + y;
-    }
-    return {
-         publicTest: function(b){
-           return addTwoNumbers(b);
-         }
-    }
+ 
 })();
 
 
@@ -23,10 +15,5 @@ var UIController = (function(){
 
 
 var controller = (function(budgetCtrl,UICtrl){
-    var z = budgetCtrl.publicTest(5)
-    return{
-        anotherPublicTest: function(){
-            console.log(z);
-        }
-    }
+    
 })(budgetController,UIController);
