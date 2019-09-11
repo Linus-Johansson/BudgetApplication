1af9f68700488924e0bd199f2d7b522be5cbbcc5
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2019-09-11 15:23:32 +0200

    added eventhandler for  add_btn

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-// this is the starter js file.. Module pattern
+// Module pattern
 
+
+//BUDGET CONTROLLER
 var budgetController = (function(){
- 
+ // some code
 })();
 
 
 
 
-
+// UI-CONTROLLER
 var UIController = (function(){
     //some code
 })();
 
 
-
+// GLOBAL-APP-CONTROLLER
 var controller = (function(budgetCtrl,UICtrl){
+
+    document.querySelector(".add__btn").addEventListener("click",function(){
+        console.log("button was clicked!")
+    });
     
 })(budgetController,UIController);
